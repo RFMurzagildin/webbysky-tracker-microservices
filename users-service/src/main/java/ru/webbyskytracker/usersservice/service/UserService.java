efcf6188861762faba472a6ba53516c8e009b61b
Ranis Murzagildin <murza.ran@mail.ru>
2026-02-15 13:16:12 +0300

    Добавили endpoint, с помощью которого пользователь сможет проверить, валидный ли у него токен на данный момент

--- users-service/src/main/java/ru/webbyskytracker/usersservice/service/UserService.java
+++ users-service/src/main/java/ru/webbyskytracker/usersservice/service/UserService.java
@@ -1,13 +1,17 @@
 package ru.webbyskytracker.usersservice.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import ru.webbyskytracker.usersservice.dto.response.ApiResponse;
 import ru.webbyskytracker.usersservice.dto.response.UserInfo;
 import ru.webbyskytracker.usersservice.entity.User;
 import ru.webbyskytracker.usersservice.exception.UserNotFoundException;
 import ru.webbyskytracker.usersservice.repository.UserRepository;
+import ru.webbyskytracker.usersservice.security.jwt.JwtService;
 
 import java.util.Optional;
 
@@ -15,6 +19,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final JwtService jwtService;
 
     public UserInfo getUserInfo(){
         String currentUserEmail = getCurrentUserEmail();
@@ -33,6 +38,22 @@ public class UserService {
 
     public Optional<User> findByEmail(String email){
         return userRepository.findByEmail(email);
+    }
+
+    public ResponseEntity<?> validateToken(String authorizationHeader){
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+            try {
+                if (jwtService.validateToken(token)) {
+                    return new ResponseEntity<>(new ApiResponse(true, "True token"), HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<>(new ApiResponse(true, "Invalid or expired token"), HttpStatus.UNAUTHORIZED);
+                }
+            } catch (Exception e) {
+                return new ResponseEntity<>(new ApiResponse(true, "Invalid token"), HttpStatus.UNAUTHORIZED);
+            }
+        }
+        return new ResponseEntity<>(new ApiResponse(true, "Missing token"), HttpStatus.UNAUTHORIZED);
     }
 
 
